47b9b53e8744aba362d6a094dab122513fb742e3
Silemo <giovanni@manfredi.ai>
2024-08-06 16:24:49 +0000

    Removed debug msg

--- crates/hdfs/src/lib.rs
+++ crates/hdfs/src/lib.rs
@@ -1,5 +1,4 @@
 use std::sync::Arc;
-use log::debug;
 
 use deltalake_core::logstore::{default_logstore, logstores, LogStore, LogStoreFactory};
 use deltalake_core::storage::{
@@ -18,7 +17,6 @@ impl ObjectStoreFactory for HdfsFactory {
         url: &Url,
         options: &StorageOptions,
     ) -> DeltaResult<(ObjectStoreRef, Path)> {
-        debug!("DBG - deltalake-hdfs : parse_url_opts \n");
         let store: ObjectStoreRef = Arc::new(HdfsObjectStore::with_config(
             url.as_str(),
             options.0.clone(),
@@ -35,14 +33,12 @@ impl LogStoreFactory for HdfsFactory {
         location: &Url,
         options: &StorageOptions,
     ) -> DeltaResult<Arc<dyn LogStore>> {
-        debug!("DBG - deltalake-hdfs : with_options \n");
         Ok(default_logstore(store, location, options))
     }
 }
 
 /// Register an [ObjectStoreFactory] for common HDFS [Url] schemes
 pub fn register_handlers(_additional_prefixes: Option<Url>) {
-    debug!("DBG - deltalake-hdfs : register_handlers \n");
     let factory = Arc::new(HdfsFactory {});
     for scheme in ["hdfs", "viewfs"].iter() {
         let url = Url::parse(&format!("{}://", scheme)).unwrap();
